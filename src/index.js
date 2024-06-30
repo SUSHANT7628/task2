af10ed279897bf134b1277b316bd6681c0c9c6fc
SUSHANT7628 <116184970+SUSHANT7628@users.noreply.github.com>
2024-06-30 16:17:40 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
+// index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import ContactListApp from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <React.StrictMode>
+        <ContactListApp />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
